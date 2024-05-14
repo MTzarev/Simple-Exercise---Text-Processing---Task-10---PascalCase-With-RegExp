187594d3ae7c31c002b1027b5d81df0e4c825224
Miroslav Tsarev <mtzarev@gmail.com>
2024-05-14 14:32:22 +0300

    Add files via upload

---
+++ test10.js
@@ -0,0 +1,7 @@
+function test10(input) {
+    let text = input.match(/[A-Z][a-z]*[a-z]+/g);
+    console.log(text.join(`, `));
+}
+test10('SplitMeIfYouCanHaHaYouCantOrYouCan')
+
+///[A-Z][a-z]*|[a-z]+/g
